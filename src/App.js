bb274122c78d5f78723fce94e631ec1b11f2c31d
superxp360 <kevinfernandez0416@gmail.com>
2023-07-03 10:15:57 -0400

    Sending Prop

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import Greeting from './components/Greeting';
 import './App.css';
+
+const students = ['Andrew', 'Ariana', 'Cihan', 'Danny', 'John', 'Kevin D.', 'Kevin', 'Max', 'Lorenzo', 'Max', 'Pat', 'Sam', 'Yaslin']
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Props Demo</h1>
+        {students.map(students => <Greeting key={students} name = {students} />)}
       </header>
     </div>
   );
